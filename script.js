9ec346bcd54f285d67eb0537cfe5d8ea6583c3f8
ParmarNirbhay <119995697+ParmarNirbhay@users.noreply.github.com>
2023-07-01 20:16:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function decimalToBinary(num) {
-  //Write you code here
+  //Write you code here  
+	function decimalToBinary(decimal) {
+      return decimal.toString(2);
+    }
+     
+    console.log(decimalToBinary(7));
+    console.log(decimalToBinary(10)); 
+    console.log(decimalToBinary(33)); 
+	
   
 }
 
